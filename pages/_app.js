fbfbe82dd536e41977b90be1722de853cedbb211
Chan Tak Chung <michaelmorningthrees@gmail.com>
2026-01-13 22:13:49 +0800

    add google tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Google tag (gtag.js) - Using Next.js Script for better performance */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17861479339"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17861479339');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
